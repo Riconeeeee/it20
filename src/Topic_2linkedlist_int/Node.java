74baa7dc7066d0666562a92352001f80744c32ed
Riconeeeee <riconeometer18@gmail.com>
2024-10-03 09:54:13 +0800

    initial commit

---
+++ src/Topic_2linkedlist_int/Node.java
@@ -0,0 +1,19 @@
+package Topic_2linkedlist_int;
+
+/**
+ *
+ * @author Erick James Rubin
+ */
+public class Node {
+    
+	int data;  
+    Node next;  
+
+    // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
